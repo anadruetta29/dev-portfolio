18c355ddfabd02cbbf0de4c019bad36fa84ddfc4
anadruetta29 <anadruetta2023@gmail.com>
2025-12-11 21:37:52 -0300

    fix: controller
    fix: controller

--- backend/src/controllers/EmailController.ts
+++ backend/src/controllers/EmailController.ts
@@ -2,32 +2,31 @@ import { Request, Response } from "express";
 import { EmailService } from "../services/email/EmailService";
 import { SendMailOptions } from "../services/email/SendMailOptions"
 
+
 export class EmailController {
-  private emailService: EmailService;
+  	private emailService: EmailService;
 
-  constructor() {
-    this.emailService = new EmailService();
-  }
 
-  send = async (req: Request, res: Response) => {
-    const { to, subject, htmlBody, attachements } = req.body as SendMailOptions;
+	constructor() {
+		this.emailService = new EmailService();
+	}
+	
+	send = async (req: Request, res: Response) => {
+		const { name, email, message } = req.body;
 
-    try {
-      const success = await this.emailService.sendEmail({
-        to,
-        subject,
-        htmlBody,
-        attachements,
-      });
 
-      if (!success) {
-        return res.status(400).json({ ok: false, msg: "Error enviando email" });
-      }
+		try {
+		const success = await this.emailService.sendContactEmail({ name, email, message });
 
-      return res.json({ ok: true, msg: "Email enviado correctamente" });
-    } catch (error) {
-      console.error("Error en EmailController:", error);
-      return res.status(500).json({ ok: false, msg: "Error interno" });
-    }
-  };
+
+		if (!success) return res.status(400).json({ ok: false, msg: "Error enviando email" });
+
+
+		return res.json({ ok: true, msg: "Mensaje enviado correctamente" });
+		} catch (error) {
+		console.error(error);
+		return res.status(500).json({ ok: false, msg: "Error interno" });
+		}
+	};
+
 }
