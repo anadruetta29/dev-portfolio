4f39aae884ba0ea600bd62b94f2400c3188f45df
anadruetta29 <anadruetta2023@gmail.com>
2025-12-11 21:31:42 -0300

    add: viewmodel front
    add: viewmodel front

--- frontend/src/ui/routes/home/viewmodel.tsx
+++ frontend/src/ui/routes/home/viewmodel.tsx
@@ -1,19 +1,25 @@
 import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { Project } from "../../../domain/entity/Project";
 
+
 export default function ViewModel() {
+
 
     // ------------------------------------------
     // SECTION SCROLL LISTENER
     // ------------------------------------------
     const [activeSection, setActiveSection] = useState("home");
 
+
     useEffect(() => {
         const sections = document.querySelectorAll("section");
+
 
         function handleScroll() {
             let current = "home";
             const scrollPos = window.scrollY + 200;
+
 
             sections.forEach((section) => {
                 if (scrollPos >= section.offsetTop) {
@@ -21,17 +27,60 @@ export default function ViewModel() {
                 }
             });
 
+
             setActiveSection(current);
         }
+
 
         window.addEventListener("scroll", handleScroll);
         return () => window.removeEventListener("scroll", handleScroll);
     }, []);
 
+
     // ------------------------------------------
     // EMAIL ACTION
     // ------------------------------------------
-    const onClickOnSendEmail = async () => {};
+    const onClickOnSendEmail = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+
+        const form = event.currentTarget;
+
+
+        try {
+            const formData = new FormData(event.currentTarget);
+const data = Object.fromEntries(formData) as {
+                name?: string;
+                email?: string;
+                message?: string;
+            };
+
+
+            const response = await fetch("http://localhost:4000/api/email/send", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data),
+            });
+
+
+            const result = await response.json();
+
+
+            if (result.ok) {
+                toast.success("Mensaje enviado correctamente");
+                event.currentTarget.reset();
+            }
+            else {
+                toast.error("Error enviando mensaje: " + result.msg);
+            }
+
+
+        } catch (error) {
+            console.error("Error enviando mensaje:", error);
+            toast.error("Error enviando mensaje");
+        }
+    };
+
 
     // ------------------------------------------
     // PROJECTS CAROUSEL
@@ -40,8 +89,7 @@ export default function ViewModel() {
         new Project(
             "ForTune",
             "ForTune es una aplicación web orientada a músicos que buscan oportunidades laborales, colaboraciones y comunicación con otros artistas. El sistema permite a los usuarios crear un perfil profesional, publicar sus habilidades, recibir propuestas, chatear en tiempo real con otros músicos, y buscar proyectos musicales acordes a su estilo.",
-            ["Java", "Typescript", "PostgreSQL", "Springboot", "React"],
-            "https://github.com/2piradrian/seminario-client.git",
+            ["Java", "Typescript", "PostgreSQL", "Springboot", "React"],"https://github.com/2piradrian/seminario-client.git",
             "2025"
         ),
         new Project(
@@ -53,7 +101,9 @@ export default function ViewModel() {
         )
     ];
 
+
     const [currentProject, setCurrentProject] = useState(0);
+
 
     const next = () => {
         triggerFade();
@@ -62,6 +112,7 @@ export default function ViewModel() {
         );
     };
 
+
     const prev = () => {
         triggerFade();
         setCurrentProject((prev) =>
@@ -69,7 +120,10 @@ export default function ViewModel() {
         );
     };
 
+
     const current = projects[currentProject];
+
+
 
 
     // ------------------------------------------
@@ -77,10 +131,13 @@ export default function ViewModel() {
     // ------------------------------------------
     const [isFading, setIsFading] = useState(false);
 
+
     const triggerFade = () => {
         setIsFading(true);
         setTimeout(() => setIsFading(false), 300);
-    };
+};
+
+
 
 
     return {
@@ -94,3 +151,5 @@ export default function ViewModel() {
         current
     };
 }
+
+
