d4760228f5d735b539218932d70a7b2348258b3f
anadruetta29 <anadruetta2023@gmail.com>
2026-01-02 19:51:53 -0300

    add: css fixing

--- frontend/src/ui/components/sections/about-me/aboute-me.tsx
+++ frontend/src/ui/components/sections/about-me/aboute-me.tsx
@@ -4,6 +4,7 @@ import cv from "../../../../../public/cv/CV - Ana Druetta.pdf"
 import githubIcon from "../../../assets/icons/github.svg";
 import docIcon from "../../../assets/icons/document.svg";
 import MainIconButton from "../../atoms/main-icon-button/main-icon-button";
+import heroImg from "../../../assets/icons/computer-and-people.svg";
 import style from "./style.module.css";
 
 type Props = {
@@ -13,16 +14,21 @@ type Props = {
 export default function AbouteMeSection({ id }: Props) {
     return (
         <section id={id} className={style.container}>
-            
-            <div className={style.leftSide}>
-
+            <FadeIn delay={0}>
+                <div className={style.illustrationSide}>
+                    <img src={heroImg} alt="" />
+                </div>
+            </FadeIn>
+            <div className={style.informationSide}>
                 <FadeIn delay={0}>
                     <div className={style.titleInformation}>
                         <div className={style.roleTag}>Desarrolladora de Software</div>
-                        <LargeTitle text="Hola! Soy Ana" />
+                        <div className={style.heroTitle}>
+                            <LargeTitle text="Hola! Soy " />
+                            <span className={style.nameHighlight}>Ana</span>
+                        </div>
                     </div>
                 </FadeIn>
-
                 <FadeIn delay={0.4}>
                     <p className={style.description}>
                         Creo sistemas con un enfoque en el detalle: limpios, rápidos y con una
@@ -30,7 +36,6 @@ export default function AbouteMeSection({ id }: Props) {
                         y memorables. Explorá mis proyectos y contactame para trabajar juntos.
                     </p>
                 </FadeIn>
-
                 <FadeIn delay={0.6}>
                     <div className={style.informationButtons}>
                         <MainIconButton 
@@ -38,7 +43,7 @@ export default function AbouteMeSection({ id }: Props) {
                             type="button"
                             enabled={true}
                             icon={githubIcon}
-                            onClick={() => window.open("https://github.com", "_blank")}
+                            onClick={() => window.open("https://github.com/anadruetta29", "_blank")}
                         />
                         <MainIconButton 
                             text="Ver currículum"
@@ -49,9 +54,7 @@ export default function AbouteMeSection({ id }: Props) {
                         />
                     </div>
                 </FadeIn>
-
             </div>
-
         </section>
     );
 }
