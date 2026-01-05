1405588ad11810567265088b176b808431062fd4
anadruetta29 <anadruetta2023@gmail.com>
2026-01-05 19:00:31 -0300

    delete backend

--- backend/src/services/email/Attachement.ts
+++
@@ -1,4 +0,0 @@
-export interface Attachement {
-  filename: string;
-  path: string;
-}
--- backend/src/services/email/SendMailOptions.ts
+++
@@ -1,8 +0,0 @@
-import type { Attachement } from "./Attachement";
-
-export interface SendMailOptions {
-    name: string;
-    email: string;
-    message: string;
-    attachments?: Attachement[];
-}
